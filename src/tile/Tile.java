80d8467505287adafc574981fec1ca3b9cd0e1c5
Vinith Chandrakumar <Chandrakumar.vinith@bene-edu.ch>
2023-07-06 15:50:52 +0200

    Map unfinished

--- src/tile/Tile.java
+++ src/tile/Tile.java
@@ -1,8 +1,8 @@
-package map;
+package tile;
 
 import java.awt.image.BufferedImage;
 
-public class Map {
+public class Tile {
 
     public BufferedImage image; // Das Bild der Kachel
     public boolean collision = false; // Gibt an, ob eine Kollision mit dieser Kachel möglich ist
