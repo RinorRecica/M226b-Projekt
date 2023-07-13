942ae7bc97416be37c254e518d3556a79dd9de27
besart jashari <jashari.besart@bene-edu.ch>
2023-07-13 18:21:01 +0200

    schwert schwingen!

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,27 +1,26 @@
 package entity;
 
-
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-
 import main.GamePanel;
 import main.Steuerung;
 
-public class Player extends Entity{
-
+public class Player extends Entity {
 	GamePanel gp;
 	Steuerung keyH;
 
 	public final int screenX;
 	public final int screenY;
 
-	public Player(GamePanel gp, Steuerung keyH) {
+	BufferedImage schwertl1; // Erstes Schwertbild
+	BufferedImage schwertl2; // Zweites Schwertbild
+	int schwertFrame = 1;    // aktueller Schwertframe
 
+	public Player(GamePanel gp, Steuerung keyH) {
 		this.gp = gp;
 		this.keyH = keyH;
 
@@ -39,18 +38,14 @@ public class Player extends Entity{
 	}
 
 	public void setDefaultValues() {
-
 		worldX = gp.tileSize * 23;
 		worldY = gp.tileSize * 21;
 		speed = 4;
 		direction = "down";
-
 	}
 
 	public void getPlayerImage() {
-
 		try {
-
 			up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
 			down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
 			left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
@@ -59,44 +54,31 @@ public class Player extends Entity{
 			down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
 			left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
 			right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
-			schwertl = ImageIO.read(getClass().getResourceAsStream("/player/holzschwert.png"));
-
-
-
-		}catch(IOException e) {
+			schwertl1 = ImageIO.read(getClass().getResourceAsStream("/player/holzschwert.png"));
+			schwertl2 = ImageIO.read(getClass().getResourceAsStream("/player/holzschwert-swing.png"));
+		} catch(IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
-	//update funktion was passiert wenn (?funktion?) taste gedrückt isz
 	public void update() {
-
-		if(keyH.oben == true || keyH.unten == true ||
-				keyH.links == true || keyH.rechts == true) {
-
-
+		if(keyH.oben == true || keyH.unten == true || keyH.links == true || keyH.rechts == true) {
 			if(keyH.oben == true) {
 				direction = "up";
 			}
 			else if(keyH.unten == true) {
 				direction = "down";
-
 			}
 			else if(keyH.links == true) {
 				direction = "left";
-
 			}
 			else if(keyH.rechts == true) {
 				direction = "right";
-
 			}
 
-			//Check TILE COLLISION
 			collisionOn = false;
 			gp.cChecker.checkTile(this);
 
-			//IF collision is FALSE, PLAYER can move
 			if(collisionOn == false) {
 				switch(direction) {
 					case"up": worldY -= speed;break;
@@ -106,14 +88,6 @@ public class Player extends Entity{
 				}
 			}
 
-
-
-			//dadurch dass wir update 60mal pro sekunde aufrufen-
-			//jede pro frame countet++ spriteCounter! dadurch dass das so schnell geschieht müüsen wir das-
-			//-runterbrechen auf alle 11 frames. alles down bellow.
-			//dadurch das diess non stop geschah mussten wir funktion UPDATE if elsen. so dass nuuuur wenn keyH(up, down. left, right)Tastegedrückt-
-			// erst dann soll es counten und Update updaten!
-
 			spriteCounter++;
 			if(spriteCounter > 10) {
 				if(spriteNum == 1) {
@@ -122,15 +96,22 @@ public class Player extends Entity{
 				else if(spriteNum == 2) {
 					spriteNum = 1;
 				}
+
+				// Schwert Animation Frame wechseln
+				if(schwertFrame == 1) {
+					schwertFrame = 2;
+				}
+				else if(schwertFrame == 2) {
+					schwertFrame = 1;
+				}
 				spriteCounter = 0;
 			}
 		}
 	}
 
 	public void draw(Graphics2D g2) {
-
 		BufferedImage image = null;
-		BufferedImage schwertImage = schwertl; // Schwertbild
+		BufferedImage schwertImage = null; // Schwertbild
 
 		switch(direction) {
 			case "up":
@@ -166,19 +147,18 @@ public class Player extends Entity{
 				}
 				break;
 		}
+
+		// Auswahl des Schwertbildes basierend auf dem aktuellen Frame
+		if(schwertFrame == 1) {
+			schwertImage = schwertl1;
+		}
+		else if(schwertFrame == 2) {
+			schwertImage = schwertl2;
+		}
+
 		// Erst das Bild des Spielers zeichnen
 		g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
 		// Dann das Schwertbild zeichnen, man muss die Koordinaten eventuell anpassen.
 		g2.drawImage(schwertImage, screenX, screenY, gp.tileSize, gp.tileSize, null);
-
-	}}
-
-
-
-
-
-
-
-
-
-
+	}
+}
